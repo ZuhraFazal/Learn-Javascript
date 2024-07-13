51fb59e03e6c9ecffbd480451bd83cab52ebc876
ZuhraFazal <107793525+ZuhraFazal@users.noreply.github.com>
2024-07-13 19:35:35 +0000

    Learned about Variables

---
+++ 01_basic/variable.js
@@ -0,0 +1,16 @@
+const MyId = 1;
+let Name = 'Zuhra';
+var age = 21;
+city = 'karachi';
+let state;
+
+// Cannot change the value of const variable
+console.log(MyId);
+
+/*
+prefer not to user var due to scope and funtion block
+js allow us to create var without a keyword but it is not preferrable
+we can declare a variable with out a value
+*/
+
+console.table([MyId,Name,age,city,state]);
