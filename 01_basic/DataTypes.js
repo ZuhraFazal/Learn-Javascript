09badb988ee246f82879ed60f4a82c4e13f0fb64
ZuhraFazal <107793525+ZuhraFazal@users.noreply.github.com>
2024-07-13 19:56:31 +0000

    learned about datatypes

---
+++ 01_basic/DataTypes.js
@@ -0,0 +1,18 @@
+// Javascript have multiple datatypes
+
+// nuumber for integer
+// BigInt for bigger nmbers
+var age = 21; 
+
+//'' or "" can be used for string
+let name = "Zuhra"
+
+//Boolan
+let isActive = true;
+
+//null is a standalone value
+//undefined is itself a datatype
+//symbol => used in react
+
+console.log(typeof age);
+console.log(typeof "")
