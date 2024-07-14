64edc0eea735df42bb19b809f5fefc345a1bb59c
ZuhraFazal <107793525+ZuhraFazal@users.noreply.github.com>
2024-07-14 10:13:09 +0000

    Learned about conversions

---
+++ 01_basic/conversion.js
@@ -0,0 +1,22 @@
+let number = 33
+console.log(number)
+console.log(typeof number)
+
+let stringNumber = String(number)
+console.log(typeof stringNumber)
+
+let mixnumber = "33abd"
+console.log(mixnumber)
+console.log(typeof mixnumber)
+
+let checkbool = true
+console.log(typeof checkbool)
+let numberBool = Number(checkbool)
+console.log(numberBool)
+
+let string = ""
+let string02 = "zuhra"
+string = Boolean(string)
+string02 = Boolean(string02)
+console.log(string)
+console.log(string02)
